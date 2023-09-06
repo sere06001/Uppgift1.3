2ee39e3b573db97da5d1c76a8c65178d4237f67c
sere06001 <sere06001@utb.vaxjo.se>
2023-09-06 10:32:31 +0200

    Add project files.

---
+++ Uppgift1.3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Uppgift1_3 
+{ 
+    class Program 
+    { 
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("   /\\");
+            Console.WriteLine("  /  \\");
+            Console.WriteLine(" / \"\" \\");
+            Console.WriteLine("/______\\");
+            Console.ReadKey();
+        }
+    }
+}
